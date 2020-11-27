748434913a592bc2e6e1cc10843a36cf59c227b6
mukoedo1993 <69023906+mukoedo1993@users.noreply.github.com>
2020-11-26 19:36:57 -0800

    Add files via upload

---
+++ effective_modern_CPP/item7/braced_initializer_case1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<cstring>
+class Widget{
+    public:
+    Widget(int ,bool)//ctors not declaring
+    {
+        std::cout<<" Widget(int ,bool)\n";
+    }
+    Widget(int,double)//std::initializer_list params
+    {
+        std::cout<<"  Widget(int,double)\n";
+    }
+    Widget(std::initializer_list<long double>il)
+    {
+        std::cout<<" Widget(std::initializer_list<long double>il)\n";
+    }
+    constexpr operator  float()const//convert to float
+    {
+        return 5.2;
+    }
+};
+int main()
+{
+ Widget w1(10,true);
+ Widget w2(10,true);
+ Widget w3(10,5.0);
+ Widget w4{10,5.0};
+}
+/*
+ Widget(int ,bool)
+ Widget(int ,bool)
+  Widget(int,double)
+ Widget(std::initializer_list<long double>il)
+*/
