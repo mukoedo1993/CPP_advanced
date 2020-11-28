c010cde8d4bc3538d301d6fcf57e4b7ea0813bce
mukoedo1993 <69023906+mukoedo1993@users.noreply.github.com>
2020-11-27 21:48:20 -0800

    Create temporay_buffer.cpp

---
+++ CPP_Standard_library/chap19/temporay_buffer.cpp
@@ -0,0 +1,35 @@
+// get/return_temporary_buffer example
+#include <iostream>
+#include <memory>
+#include <algorithm>
+
+int main () {
+  int numbers[] = {30,50,10,20,60,40};
+
+  // use temporary buffer to sort and show the numbers:
+  std::pair <int*,std::ptrdiff_t> result = std::get_temporary_buffer<int>(6);
+  if (result.second>0)
+  {
+    std::uninitialized_copy (numbers,numbers+result.second,result.first);
+    std::sort (result.first,result.first+result.second);
+    std::cout << "sorted numbers  : ";
+    for (int i=0;i<result.second;i++)
+      std::cout << result.first[i] << " ";
+    std::cout << '\n';
+    std::return_temporary_buffer (result.first);
+  }
+
+  // show original numbers:
+  std::cout << "unsorted numbers: ";
+  for (int i=0;i<6;i++)
+    std::cout << numbers[i] << " ";
+  std::cout << '\n';
+  return 0;
+}
+
+/*
+possible output:
+
+sorted numbers  : 10 20 30 40 50 60
+unsorted numbers: 30 50 10 20 60 40
+*/
